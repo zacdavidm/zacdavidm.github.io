f166ad3813c5d5a6127a88408890133251a38302
Zac DavidM <zacdavidm@gmail.com>
2015-06-24 14:01:45 -0400

    add done function to todo controller

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
 var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) {
 	$scope.message = 'Angular is pretty cool.';
+	
+	$scope.todos = [
+    'Learn Sketch', 
+    'Look at Dribbble and feel inferior',
+    'Actually learn how to use the Pen tool'
+    ];
+    
+    $scope.done = function(todo) {
+    var indexOf = $scope.todos.indexOf(todo);
+    if (indexOf !== -1) {
+      $scope.todos.splice(indexOf, 1);
+    }
 });
