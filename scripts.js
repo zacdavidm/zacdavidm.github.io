fc9a1769dc3e6dba852593693902cf204fc78408
Zac DavidM <zacdavidm@gmail.com>
2015-06-25 13:58:27 -0400

    add get todos

--- scripts.js
+++ scripts.js
@@ -14,11 +14,17 @@ var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) {
 	$scope.message = 'Angular is pretty cool.';
 	
-	$scope.todos = [
-    'Learn Sketch', 
-    'Look at Dribbble and feel inferior',
-    'Actually learn how to use the Pen tool'
-    ];
+	var todosStorage=localStorage.getItem("todos");
+	
+	if(todosStorage!=null){
+	  $scope.todos=todosStorage;
+	}else{
+	  $scope.todos = [
+      'Learn Sketch', 
+      'Look at Dribbble and feel inferior',
+      'Actually learn how to use the Pen tool'
+      ];
+    }
     
     $scope.done = function(todo) {
     	var indexOf = $scope.todos.indexOf(todo);
